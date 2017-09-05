686af84f9115e9cd1eecaca7ceaa2cd3113f485b
RuanV <30688648+RuanV@users.noreply.github.com>
2017-09-05 16:49:48 +0200

    Add files via upload

---
+++ Activitylog.java
@@ -0,0 +1,30 @@
+Activity login:
+android:orientation=”vertical”
+android:layout_height=”match_parent”
+android:padding=”10dp”
+android:layout_width=”match_parent”
+
+<TextView
+	android:layout_width=”wrap_content”
+	android:text=”Username”
+	android:layout_height=” wrap_content” />
+<EditText
+	android:id=”@+id/etUsername”
+android:layout_width=”match_parent”
+	android:layout_marginBotton=” 10dp”
+	android:layout_height=” wrap_content” />
+<TextView
+	android:layout_width=”wrap_content”
+	android:text=”Password”
+	android:layout_height=” wrap_content” />
+<EditText
+	android:id=”@+id/etPassword”
+android:layout_width=”match_parent”
+	android:layout_marginBotton=” 10dp”
+	android:inputType=”t extPassword”
+	android:layout_height=” wrap_content” />
+<Button 
+	android:id=”@+id/bLogin
+android:text=”Login”
+android:layout_width=”match_parent”
+	android:layout_height=” wrap_content” />
